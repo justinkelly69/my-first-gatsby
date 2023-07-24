7555bbc30e882c701edeba53fda054464cb16d85
Justin Kelly <justinkelly@justinkelly.net>
2023-07-24 17:45:40 +0200

    added files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,21 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `my first gatsby`,
+    title: `My First Gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `The Greatest Site Ever`,
   },
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
 
